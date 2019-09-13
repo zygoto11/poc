291b11247533ad2d27a6293b45881ca6dd4c04c7
zygoto11 <42461385+zygoto11@users.noreply.github.com>
2019-09-13 14:29:50 +0200

    add

--- controllers/profile.js
+++ controllers/profile.js
@@ -8,10 +8,31 @@ app.controller("logCtrl", function ($scope,$http, $routeParams,$localStorage,$ti
 
 app.controller("privacyCtrl", function ($scope,$http, $routeParams,$localStorage,$timeout) {
 	$scope.userid = $localStorage.currentUser.id;
-
+	$http.get("https://zygotopoc.westeurope.cloudapp.azure.com/profile/getprivacy.php")
+    .then(function(response) {
+        $scope.privacy = response.data.result[0];
+	});
+	
+	$scope.updateprivacy = function(city,birthday,online){		
+		$http.post('https://zygotopoc.westeurope.cloudapp.azure.com/profile/updateprivacy.php',{city:city,birthday:birthday,online:online})
+        .then(function (response) {
+			$scope.msg = "Changements enregistrés !"
+		});		
+	}
 });
 
 app.controller("notificationsCtrl", function ($scope,$http, $routeParams,$localStorage,$timeout) {
 	$scope.userid = $localStorage.currentUser.id;
-
+	$http.get("https://zygotopoc.westeurope.cloudapp.azure.com/profile/getnotifications.php")
+    .then(function(response) {
+        $scope.notifications = response.data.result[0];
+	});
+	
+	$scope.updatenotifications = function(register,unregister,comment){		
+		$http.post('https://zygotopoc.westeurope.cloudapp.azure.com/profile/updatenotifications.php',{register:register,unregister:unregister,comment:comment})
+        .then(function (response) {
+			$scope.msg = "Changements enregistrés !"
+		});		
+	}
+	
 });
