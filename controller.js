50d997d547ffc438a740a2e47f4faef7d49980df
zygoto11 <42461385+zygoto11@users.noreply.github.com>
2019-09-13 11:24:45 +0200

    add

--- controller.js
+++ controller.js
@@ -407,25 +407,6 @@ app.controller("profileCtrl", function ($scope,$http, $routeParams,$localStorage
 $http.get("https://zygoto11.github.io/poc/data/cities.json")
     .then(function(response) {
         $scope.cities = response.data;
-			var $input = $(".typeahead");
-			$input.typeahead({
-			  source: $scope.cities,
-			  autoSelect: true
-			});
-			$input.change(function() {
-			  var current = $input.typeahead("getActive");
-			  if (current) {
-				// Some item from your model is active!
-				if (current.name == $input.val()) {
-				  // This means the exact match is found. Use toLowerCase() if you want case insensitive match.
-				} else {
-				  // This means it is only a partial match, you can either add a new item
-				  // or take the active if you don't want new items
-				}
-			  } else {
-				// Nothing is active so it is a new value (or maybe empty value)
-			  }
-			});	
 });
 
 
