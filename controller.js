f007b47dc4a6863e889d5fe1f2c0dcdccd3f650d
zygoto11 <42461385+zygoto11@users.noreply.github.com>
2019-09-14 15:04:28 +0200

    add

--- controller.js
+++ controller.js
@@ -7,11 +7,11 @@ app.config(function($routeProvider) {
     })
     .when("/sorties", {
         templateUrl : "sorties.html",
-		controller : "sortiesCtrl"
+		controller : "eventsCtrl"
     })
 	.when("/eventsarchives", {
         templateUrl : "events_archive.html",
-		controller : "eventsCtrl"
+		controller : "pasteventsCtrl"
     })
 	.when("/newsortie", {
         templateUrl : "newevent.html",
@@ -36,7 +36,7 @@ app.config(function($routeProvider) {
     })
 	.when("/sorties/:sortie", {
         templateUrl : "sorties.html",
-		controller : "sortiesCtrl"
+		controller : "eventsCtrl"
     })
 	.when("/profile", {
         templateUrl : "profile.html",
@@ -123,142 +123,7 @@ app.run(function($rootScope, $http, $location, $localStorage) {
 });
 
 
-app.controller("sortiesCtrl", function ($scope,$http, $location,$routeParams,$route,$localStorage) {
-	$scope.showsorties = true;
-	$scope.shownewsortie = false;
-	$scope.showdeletesortie = true;
-	$scope.userid = $localStorage.currentUser.id;
-	$scope.username = $localStorage.currentUser.username;
-	
-	$http.get("https://zygotopoc.westeurope.cloudapp.azure.com/events/getevents.php")
-    .then(function(response) {
-    $scope.sorties = response.data;
-	$scope.currentPage = 1;
-    $scope.itemsPerPage = 5;
-    $scope.maxSize = 5;
-    //$scope.totalItems = $scope.nextsorties.length;
-	
-	$scope.nextsorties = $scope.sorties.filter(function (sortie) {		
-		return ( moment(sortie.timestamp).valueOf() > Date.now());	
-	}); 
-	
-	$scope.totalItems = $scope.nextsorties.length;
-	
-    });
 
-	
-	$http.get("https://zygotopoc.westeurope.cloudapp.azure.com/?action=listusers")
-    .then(function(response) {
-        $scope.users = response.data;
-    });
-	
-	
-	
-	if($location.path()=="/newsortie"){
-		$scope.shownewsortie = true;
-		$scope.showsorties = false;
-		$scope.showdeletesortie = true;
-		
-		//default values for form
-		$scope.newsortie = {};
-		$scope.newsortie.scope = "public";
-
-  
-   $scope.poilist = [{'name':'Celt','location':'rue d\'armagnac'},
-   {'name':'Divine Comédie','location':'rue de la zaza'},
-   {'name':'la cavayere','location':'route du lac'},
-   {'name':'paicherou','location':'à côté du tennis'}
-];
-  
-		$scope.createsortie = function(name,desc,date,time,icon,scope){
-			
-			//"2018-08-07T22:00:00.000Z"
-			
-			var sortiedate = moment(date).format("YYYY-MM-DD");
-			var sortietime = moment(time).format("HH:mm");
-			var timestamp = moment(sortiedate + ' ' + sortietime);
-			
-			  $http.post('https://zygotopoc.westeurope.cloudapp.azure.com/', { name: name, desc:desc,date:sortiedate,time:sortietime,timestamp:timestamp,icon:icon,scope:scope, leader: $scope.userid,action:"createsortie" })
-             .then(function (response) {
-					
-					 $location.path('/sorties');
-			
-				 }); 
-			
-		}
-  
-  
-		
-	}
-	
-	
-	if ($routeParams.sortie) {
-		
-		$scope.sortie = $routeParams.sortie;
-		$scope.showsorties = false;
-		$scope.showdeletesortie = false;
-		
-		
-		$http.get("https://zygotopoc.westeurope.cloudapp.azure.com/?action=describesortie&id="+$scope.sortie)
-			.then(function(response) {
-			$scope.descsortie = response.data;
-		});
-		
-		$http.get("https://zygotopoc.westeurope.cloudapp.azure.com/?action=listinscriptions&sortieid="+$scope.sortie)
-			.then(function(response) {
-			$scope.inscriptions = response.data;
-		});
-		
-		$http.get("https://zygotopoc.westeurope.cloudapp.azure.com/?action=listcomments&sortieid="+$scope.sortie)
-			.then(function(response) {
-			$scope.comments = response.data;
-		});
-		
-		
-		$scope.register = function(sortie){
-			
-			$http.get('https://zygotopoc.westeurope.cloudapp.azure.com/?action=registersortie&sortieid='+sortie+'&userid='+$scope.userid)
-            .then(function (response) {					
-					$route.reload();	
-				});
-			
-		}
-		
-		$scope.unregister = function(sortie){
-			
-			$http.get('https://zygotopoc.westeurope.cloudapp.azure.com/?action=unregistersortie&sortieid='+sortie+'&userid='+$scope.userid)
-            .then(function (response) {					
-					$route.reload();	
-				});
-			
-		}
-		
-		$scope.addcomment = function(sortie,comment){
-			
-			$http.post('https://zygotopoc.westeurope.cloudapp.azure.com/',{ userid: $scope.userid , sortieid:sortie,comment:comment,action:"addcomment" })
-            .then(function (response) {					
-					$route.reload();	
-				});
-			
-		}
-		
-		
-		$scope.deletesortie = function(sortie){
-			
-			$http.get('https://zygotopoc.westeurope.cloudapp.azure.com/?action=deletesortie&id='+sortie)
-            .then(function (response) {					
-					$location.path('/sorties');
-					$route.reload();				
-			
-				});
-			
-		}
-		
-		
-		}
-		
-
-});
 
 
 app.controller("membersCtrl", function ($scope,$http, $routeParams,$location,$route,$localStorage) {
