1bdc003814c79646a378265cdc80d018b26ee13d
zygoto11 <42461385+zygoto11@users.noreply.github.com>
2019-09-14 15:04:04 +0200

    add

--- controllers/events.js
+++ controllers/events.js
@@ -11,7 +11,13 @@ app.controller("eventsCtrl", function ($scope,$http, $location,$routeParams,$rou
 	$scope.currentPage = 1;
     $scope.itemsPerPage = 5;
     $scope.maxSize = 5;
-    $scope.totalItems = $scope.sorties.length;
+    //$scope.totalItems = $scope.nextsorties.length;
+	
+	$scope.nextsorties = $scope.sorties.filter(function (sortie) {		
+		return ( moment(sortie.timestamp).valueOf() > Date.now());	
+	}); 
+	
+	$scope.totalItems = $scope.nextsorties.length;
 	
     });
 
@@ -86,9 +92,10 @@ app.controller("eventsCtrl", function ($scope,$http, $location,$routeParams,$rou
 		
 		$scope.register = function(sortie){
 			
-			$http.get('https://zygotopoc.westeurope.cloudapp.azure.com/?action=registersortie&sortieid='+sortie+'&userid='+$scope.userid)
-            .then(function (response) {					
-					$route.reload();	
+			$http.post('https://zygotopoc.westeurope.cloudapp.azure.com/events/registerevent.php?',{eventid:sortie})
+            .then(function (response) {	
+				$scope.reg = response.data;
+				$route.reload();	
 				});
 			
 		}
@@ -125,6 +132,74 @@ app.controller("eventsCtrl", function ($scope,$http, $location,$routeParams,$rou
 		
 		
 		}
+		
+
+});
+
+
+app.controller("pasteventsCtrl", function ($scope,$http, $location,$routeParams,$route,$localStorage) {
+	$scope.showsorties = true;
+	$scope.shownewsortie = false;
+	$scope.showdeletesortie = true;
+	$scope.userid = $localStorage.currentUser.id;
+	$scope.username = $localStorage.currentUser.username;
+	
+	$http.get("https://zygotopoc.westeurope.cloudapp.azure.com/events/getevents.php")
+    .then(function(response) {
+    $scope.sorties = response.data;
+	$scope.currentPage = 1;
+    $scope.itemsPerPage = 5;
+    $scope.maxSize = 5;
+    $scope.totalItems = $scope.sorties.length;
+	
+    });
+
+	
+	$http.get("https://zygotopoc.westeurope.cloudapp.azure.com/?action=listusers")
+    .then(function(response) {
+        $scope.users = response.data;
+    });
+	
+	
+	
+	if($location.path()=="/newsortie"){
+		$scope.shownewsortie = true;
+		$scope.showsorties = false;
+		$scope.showdeletesortie = true;
+		
+		//default values for form
+		$scope.newsortie = {};
+		$scope.newsortie.scope = "public";
+
+  
+   $scope.poilist = [{'name':'Celt','location':'rue d\'armagnac'},
+   {'name':'Divine Comédie','location':'rue de la zaza'},
+   {'name':'la cavayere','location':'route du lac'},
+   {'name':'paicherou','location':'à côté du tennis'}
+];
+  
+		$scope.createsortie = function(name,desc,date,time,icon,scope){
+			
+			//"2018-08-07T22:00:00.000Z"
+			
+			var sortiedate = moment(date).format("YYYY-MM-DD");
+			var sortietime = moment(time).format("HH:mm");
+			var timestamp = moment(sortiedate + ' ' + sortietime);
+			
+			  $http.post('https://zygotopoc.westeurope.cloudapp.azure.com/', { name: name, desc:desc,date:sortiedate,time:sortietime,timestamp:timestamp,icon:icon,scope:scope, leader: $scope.userid,action:"createsortie" })
+             .then(function (response) {
+					
+					 $location.path('/sorties');
+			
+				 }); 
+			
+		}
+  
+  
+		
+	}
+	
+	
 		
 
 });
