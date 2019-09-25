e0fbe8f7897af1a20e59abc57c139183c6677b1a
zygoto11 <42461385+zygoto11@users.noreply.github.com>
2019-09-25 21:03:37 +0200

    add

--- controllers/forums.js
+++ controllers/forums.js
@@ -10,27 +10,105 @@ app.controller("forumsCtrl", function ($scope,$route,$http, $routeParams,$localS
         $scope.topics = response.data;		
 		});		
 	
+				$http.get("https://zygotopoc.westeurope.cloudapp.azure.com/members/getusers.php")
+    .then(function(response) {
+       $scope.users = response.data.result.users;
+	$scope.online = response.data.result.online;
+    });
+	
 		
 		
 		}
-		
-		
-		if ($routeParams.newtopic) {
-			
-			
-			if ($routeParams.forumid) {
-			$scope.shownewtopic = true;
-			$scope.showforums = false;
-			}
-			
-		}
-		
-		
-		
-		
-		
 
 	
+});
+
+
+app.controller("topicCtrl", function ($scope,$route,$http, $routeParams,$localStorage,$location) {
+		if ($routeParams.topicid) {
+		
+		$scope.topicid = $routeParams.topicid;
+		$scope.userid = $localStorage.currentUser.id;
+		
+		$http.get("https://zygotopoc.westeurope.cloudapp.azure.com/forums/gettopic.php?id="+$scope.topicid)
+		.then(function(response) {
+        $scope.topic = response.data;		
+		});		
 	
+				$http.get("https://zygotopoc.westeurope.cloudapp.azure.com/members/getusers.php")
+    .then(function(response) {
+       $scope.users = response.data.result.users;
+	$scope.online = response.data.result.online;
+    });
+	
+	
+	 $scope.tinyMceOptions = {
+                        plugins: ['lists','emoticons template paste textcolor textpattern imagetools'],
+                        statusbar: false,
+                        menubar: false,
+                        resize: false,
+                        language: 'fr_FR',
+						readonly : 0,
+                        language_url:'https://cdn.jsdelivr.net/npm/tinymce-lang@0.0.1/langs/fr_FR.js',
+                        toolbar: 'bold italic underline | bullist numlist | alignleft aligncenter alignright | undo redo | forecolor backcolor'
+
+                    };
+		
+		$scope.replytopic = function(msgcontent){
+			
+			$http.post('https://zygotopoc.westeurope.cloudapp.azure.com/forums/replytopic.php',{ msgid:$scope.topicid,msgcontent:msgcontent})
+            .then(function (response) {					
+				$route.reload();		
+				});
+			
+		}
+	
+		
+		
+		}
+
+	
+});
+
+
+app.controller("newtopicCtrl", function ($scope,$route,$http, $routeParams,$localStorage,$location) {
+		if ($routeParams.forumid) {
+		
+
+		$scope.forumid = $routeParams.forumid;
+	
+
+		
+			 $scope.tinyMceOptions = {
+                        plugins: ['lists','emoticons template paste textcolor textpattern imagetools'],
+                        statusbar: false,
+                        menubar: false,
+                        resize: false,
+                        language: 'fr_FR',
+						readonly : 0,
+                        language_url:'https://cdn.jsdelivr.net/npm/tinymce-lang@0.0.1/langs/fr_FR.js',
+                        toolbar: 'bold italic underline | bullist numlist | alignleft aligncenter alignright | undo redo | forecolor backcolor'
+
+                    };
+					
+					
+						$scope.createtopic = function(title,content){			
+			 $http.post('https://zygotopoc.westeurope.cloudapp.azure.com/forums/createtopic.php',{  forumid:$scope.forumid,title:title,content:content })
+            .then(function (response) {
+				if (response.data.code !== 200) {
+					$scope.error_msg = response.data.message;
+				}
+				else{
+					
+					$location.path('/forums/'+$scope.forumid);	
+				}
+				}); 
+			
+	}
+		
+		
+		
+		}
+
 	
 });
